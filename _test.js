e0250019d6c10f32f61bc683750320273bd3279e
evgeniyGusev <55669490+evgeniyGusev@users.noreply.github.com>
2022-07-19 13:44:42 +0300

    Delete _test.js

--- _test.js
+++
@@ -1,6 +0,0 @@
-import { keysParser } from './build/index.js';
-
-console.log(
-  '"{ a: { b: { c: \'да\' } } }" :: result ::',
-  keysParser('a.b.gg', { a: { b: { name: 'Pidralkin', is_pidor: 'нет', sosetInDayCount: 32 } } })
-);
