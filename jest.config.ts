2e0e997d8117e9ab1513742383a82f5d963ba751
pirateJack <shchelkalinevgeniy@gmail.com>
2022-07-20 21:24:58 +0300

    first vitests

--- jest.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  clearMocks: true,
-  coverageDirectory: './coverage',
-  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
-  preset: 'ts-jest',
-};
--- src/tests/errorHandler.test.ts
+++ src/tests/errorHandler.test.ts
@@ -1,7 +1,14 @@
 import { throwError } from '../modules/errorHandler';
+import { describe, it, expect } from 'vitest';
 
-test('Correct error message without one of parameters', () => {
-  expect(throwError({ name: 'emptyString' })).toBe("keysParser ERROR: Parameter <string> can't be empty");
-  expect(throwError({ name: 'emptyObj' })).toBe('keysParser ERROR: Parameter <obj> must contain at least one key');
-  expect(throwError({ name: 'some problem' })).toBe('keysParser ERROR: some problem');
+describe('Error messages', () => {
+  it('Correct error message without emptyString parameter', () => {
+    expect(throwError({ name: 'emptyString' })).toBe("keysParser ERROR: Parameter <string> can't be empty");
+  });
+  it('Correct error message without emptyObj parameter', () => {
+    expect(throwError({ name: 'emptyObj' })).toBe('keysParser ERROR: Parameter <obj> must contain at least one key');
+  });
+  it('Correct error message without some parameter', () => {
+    expect(throwError({ name: 'some problem' })).toBe('keysParser ERROR: some problem');
+  });
 });
