a5e95884418f52b0df7713e29d638d1b9f6cbb28
evgeniyGusev <55669490+evgeniyGusev@users.noreply.github.com>
2022-07-21 17:57:52 +0300

    Switch tests to vitest (#3)
    * first vitests
    
    * update tests

--- src/tests/errorHandler.test.ts
+++ src/tests/errorHandler.test.ts
@@ -1,7 +1,16 @@
 import { throwError } from '../modules/errorHandler';
+import { defaults } from './config/index';
 
-test('Correct error message without one of parameters', () => {
-  expect(throwError({ name: 'emptyString' })).toBe("keysParser ERROR: Parameter <string> can't be empty");
-  expect(throwError({ name: 'emptyObj' })).toBe('keysParser ERROR: Parameter <obj> must contain at least one key');
-  expect(throwError({ name: 'some problem' })).toBe('keysParser ERROR: some problem');
+const { describe, expect, it } = defaults;
+
+describe('Error messages', () => {
+  it('Correct error message without emptyString parameter', () => {
+    expect(throwError({ name: 'emptyString' })).toBe("keysParser ERROR: Parameter <string> can't be empty");
+  });
+  it('Correct error message without emptyObj parameter', () => {
+    expect(throwError({ name: 'emptyObj' })).toBe('keysParser ERROR: Parameter <obj> must contain at least one key');
+  });
+  it('Correct error message without some parameter', () => {
+    expect(throwError({ name: 'some problem' })).toBe('keysParser ERROR: some problem');
+  });
 });
